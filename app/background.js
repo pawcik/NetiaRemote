fbe49763038c966bc5bbe629e5f2f7ea4b417906
pawcik <pawcik@gmail.com>
2014-02-22 22:50:27 +0100

    discover netia player

--- app/background.js
+++ app/background.js
@@ -23,54 +23,33 @@ var ab2str=function(buf) {
 };
 
 // Create the Socket
-chrome.socket.create('udp', null, function(createInfo) {
-    clientSocket = createInfo.socketId;
-
-    chrome.socket.connect(clientSocket, '239.255.255.250', 1900, function(result) {
-        console.log('connect result = ' + result.toString());
-    });
-
-    // We are now connected to the socket so send it some data
-    chrome.socket.write(clientSocket, str2ab('M-SEARCH * HTTP/1.1\r\nHOST: 239.255.255.250:1900\r\nST: urn:netgem:RemoteControlServer:1\r\nMAN: \"ssdp:discover\"\r\nMX: 3\r\n'), function(writeInfo) {
-         console.log("writeInfo = " + writeInfo.bytesWritten);
-    });
-
-    chrome.socket.read(clientSocket, 1900, function(readInfo) {
-        console.log('Client: recived response: ' + ab2str(readInfo.data));
-    });
-});
+chrome.socket.create('udp', {}, function(socketInto) {
+    clientSocket = socketInto.socketId;
 
 
-// Server
-chrome.socket.create('udp', null, function(createInfo){
-    serverSocket = createInfo.socketId;
-
-    chrome.socket.bind(serverSocket, '0.0.0.0', 1900, function(result){
+    chrome.socket.bind(clientSocket, '0.0.0.0', 0, function(result){
+        if (result !== 0) {
+            throw('cannot bind socket')
+        }
         console.log('chrome.socket.bind: result = ' + result.toString());
     });
 
-    chrome.socket.joinGroup(serverSocket, '239.255.255.250', function(result) {
-        console.log('chrome.socket.join: result = ' + result.toString());
-    });
 
-    function read()
-    {
-        chrome.socket.recvFrom(serverSocket, 1024, function(recvFromInfo){
-            console.log('Server: recvFromInfo: ', recvFromInfo, 'Message: ', 
-                ab2str(recvFromInfo.data));
-            if(recvFromInfo.resultCode >= 0)
-            {
-                chrome.socket.sendTo(serverSocket, 
-                    str2ab('Received message from client ' + recvFromInfo.address + 
-                    ':' + recvFromInfo.port.toString() + ': ' + 
-                    ab2str(recvFromInfo.data)), 
-                    recvFromInfo.address, recvFromInfo.port, function(){});
-                read();
-            }
-            else
-                console.error('Server read error!');
-        });
+
+
+    if (!!clientSocket === false) {
+        throw('socket id is not allocated')
     }
 
-    read();
+    chrome.socket.recvFrom(clientSocket, function(recv) {
+        console.log('recv data: ' + ab2str(recv.data) )
+
+    });
+
+    var MSEARCH = 'M-SEARCH * HTTP/1.1\r\nHOST: 239.255.255.250:1900\r\nST: urn:netgem:RemoteControlServer:1\r\nMAN: \"ssdp:discover\"\r\nMX: 3\r\n'
+    chrome.socket.sendTo(clientSocket, str2ab(MSEARCH), '239.255.255.250', 1900, function(e) {
+        console.log('byte written = ' + e.bytesWritten)
+    });
+
 });
+
